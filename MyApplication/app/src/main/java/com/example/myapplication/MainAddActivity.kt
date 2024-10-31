1e34f820d405bdecbb2ee542c46b929841ee9038
Jkite0917 <deadvangers@gmail.com>
2024-10-31 22:12:06 +0900

    2024/10/31 22:11 page change menubar code update & all apply 2

---
+++ MyApplication/app/src/main/java/com/example/myapplication/MainAddActivity.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class MainAddActivity : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.main_add, container, false) // 변경: layout 파일 이름에 맞게 수정
+    }
+}
